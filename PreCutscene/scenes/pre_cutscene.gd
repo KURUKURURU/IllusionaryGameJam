3c3a483ec81696fd40fcfc4d8d0847d9f983985f
KURUKURURU <93101194+KURUKURURU@users.noreply.github.com>
2025-11-28 02:09:06 -0500

    replaced files placeholder

--- PreCutscene/scenes/pre_cutscene.gd
+++ PreCutscene/scenes/pre_cutscene.gd
@@ -4,10 +4,11 @@ func wait(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
 	
 func _ready() -> void:
+	
+	
 	$Next.hide()
-	await wait(10.0)
-	$Next.show()
-
+	await wait(4.0)
+	get_tree().change_scene_to_file("uid://b1w8iudqm0htd")
 
 func _on_next_pressed() -> void:
 	get_tree().change_scene_to_file("uid://b1w8iudqm0htd")
